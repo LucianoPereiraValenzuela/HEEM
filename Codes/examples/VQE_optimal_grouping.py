da5493a8b0b5a9c6f165d0c009730dce631e752c
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2022-01-23 18:34:48 +0100

    Minor improvements VQE run

--- Codes/examples/VQE_optimal_grouping.py
+++ Codes/examples/VQE_optimal_grouping.py
@@ -1,29 +1,33 @@
 import sys
+import warnings
+from sympy.utilities.exceptions import SymPyDeprecationWarning
+from h5py.h5py_warnings import H5pyDeprecationWarning
 
+warnings.filterwarnings("ignore", category=SymPyDeprecationWarning)
+warnings.filterwarnings("ignore", category=H5pyDeprecationWarning)
 sys.path.append('../')
 
-import numpy as np
+with warnings.catch_warnings():
+	warnings.simplefilter("ignore")
 
-from VQE import VQE
-from utils import number2SummedOp, save_object, question_overwrite
+	import numpy as np
+	from VQE import VQE
+	from utils import save_object, change_order_qubitop, current_time
 
-import os
-from tqdm import tqdm
-from joblib import Parallel, delayed
-import warnings
+	import os
+	from tqdm import tqdm
+	from joblib import Parallel, delayed
+	import getopt
 
-from qiskit import IBMQ
-from qiskit import Aer
-from qiskit.providers.aer import QasmSimulator
-from qiskit.providers.aer.noise import NoiseModel
-from qiskit.utils.quantum_instance import QuantumInstance
-from qiskit.algorithms import NumPyMinimumEigensolver
-from qiskit.algorithms.optimizers import SPSA, COBYLA
-from qiskit.ignis.mitigation.measurement import CompleteMeasFitter
-from qiskit.circuit.library import EfficientSU2
-
-save = True
-clean_start = False
+	from qiskit import IBMQ
+	from qiskit import Aer
+	from qiskit.providers.aer import QasmSimulator
+	from qiskit.providers.aer.noise import NoiseModel
+	from qiskit.utils.quantum_instance import QuantumInstance
+	from qiskit.algorithms import NumPyMinimumEigensolver
+	from qiskit.algorithms.optimizers import SPSA, COBYLA
+	from qiskit.ignis.mitigation.measurement import CompleteMeasFitter
+	from qiskit.circuit.library import EfficientSU2
 
 
 def callback(evals, params, mean, deviation):
@@ -34,138 +38,172 @@ def callback(evals, params, mean, deviation):
 
 def run_VQE(solver, qubitOp, seed, nmax=100):
 	np.random.seed(seed)
-
-	solution = False
-	while not solution and nmax > 0:
-		try:
-			solver.compute_minimum_eigenvalue(qubitOp)
-			solution = True
-		except Exception:
-			print('Trying again...')
-			nmax -= 1
-
-	if not solution:
-		return None
+	solver.compute_minimum_eigenvalue(qubitOp)
 
 	return solver.energies
 
 
 os.environ['QISKIT_IN_PARALLEL'] = 'TRUE'
 
-if len(sys.argv) == 1:
-	# print('Introduce a molecule to compute the best grouping')
-	# molecule = None
-	molecule = 'H2O'
-else:
-	molecule = str(sys.argv[1])
+# ------------  Default parameters calculation  --------------------
+save = True
+clean_start = True
 
-print('Computing optimal VQE for', molecule)
+molecule_name = 'H2O'
+n_jobs = -1
+NUM_SHOTS = 2 ** 13
+N_runs = 10
+maxiter = 300
 
-N_backup = os.cpu_count()
-N_runs = N_backup * 20
-
-NUM_SHOTS = 2 ** 13  # Number of shots for each circuit
-maxiter = 100
-
-with warnings.catch_warnings():
-	warnings.simplefilter("ignore")
-	IBMQ.load_account()
-
-provider = IBMQ.get_provider(hub='ibm-q-csic', group='internal', project='iff-csic')
 name_backend = 'ibmq_montreal'
-backend_device = provider.get_backend(name_backend)
-backend_sim = Aer.get_backend('aer_simulator')
+backend_parallel = 'loky'
+optimizer = SPSA
 
-device = QasmSimulator.from_backend(backend_device)
-coupling_map = device.configuration().coupling_map
-noise_model = NoiseModel.from_backend(device)
-basis_gates = noise_model.basis_gates
+# ---------------------------------------------------------
+message_help = 'VQE_optimal_grouping.py -m <molecule ({})> -j <#JOBS ({})> -s <# shots ({})> ' \
+               '-N <# runs ({})>, -i <# iterations ({})>, -c <clear start>'.format(molecule_name,
+                                                                                   n_jobs,
+                                                                                   NUM_SHOTS,
+                                                                                   N_runs,
+                                                                                   maxiter,
+                                                                                   clean_start)
 
-qi = QuantumInstance(backend=backend_sim,
-					 coupling_map=coupling_map,
-					 noise_model=noise_model,
-					 basis_gates=basis_gates,
-					 shots=NUM_SHOTS,
-					 measurement_error_mitigation_cls=CompleteMeasFitter)
-# cals_matrix_refresh_period=15)
+try:
+	argv = sys.argv[1:]
+	opts, args = getopt.getopt(argv, "hm:j:s:N:i:c:")
+except getopt.GetoptError:
+	print(message_help)
+	sys.exit(2)
 
-data = np.load('../data/optimal_grouping_' + molecule + '_' + name_backend + '.npy', allow_pickle=True).item()
-qubit_op = number2SummedOp(data['optimal_labels'], data['optimal_coeffs'])
+for opt, arg in opts:
+	if opt == '-h':
+		print(message_help)
+		sys.exit(2)
+	elif opt == '-m':
+		molecule_name = str(arg)
+	elif opt == '-j':
+		n_jobs = int(arg)
+	elif opt == '-s':
+		NUM_SHOTS = int(arg)
+	elif opt == '-N':
+		N_runs = int(arg)
+	elif opt == '-i':
+		maxiter = int(arg)
+	elif opt == '-c':
+		clean_start = bool(arg)
 
-result_exact = NumPyMinimumEigensolver().compute_minimum_eigenvalue(qubit_op)
-init_state = data['init_state']
+file_name = 'VQE_' + molecule_name + '_' + name_backend
 
-num_qubits = qubit_op.num_qubits
-ansatz = init_state.compose(EfficientSU2(num_qubits, ['ry', 'rz'], entanglement='linear', reps=1))
-num_var = ansatz.num_parameters
-initial_params = [0.1] * num_var
+if n_jobs == -1:
+	n_jobs = os.cpu_count()
 
-optimizer = SPSA(maxiter=maxiter, last_avg=1)
-# optimizer = COBYLA(maxiter=maxiter)
+N_runs = int(np.ceil(N_runs / n_jobs) * n_jobs)
 
-file_name = 'VQE_' + molecule + '_' + name_backend
+if __name__ == '__main__':
+	print('Code starts at:', current_time())
+	print('Computing optimal VQE for', molecule_name)
 
-N_chunks = int(np.ceil(N_runs / N_backup))
+	with warnings.catch_warnings():
+		warnings.simplefilter("ignore")
+		IBMQ.load_account()
 
-if clean_start and save:
-	try:
-		os.remove('../data/' + file_name + '.npy')
-	except Exception:
-		pass
+	provider = IBMQ.get_provider(hub='ibm-q-csic', group='internal', project='iff-csic')
+	backend_device = provider.get_backend(name_backend)
+	backend_sim = Aer.get_backend('aer_simulator')
 
-pbar = tqdm(total=N_chunks, desc='Computing VQE', file=sys.stdout, ncols=90,
-			bar_format='{l_bar}{bar}{r_bar}')
-for i in range(0, N_chunks):
-	initial = i * N_backup
-	final = min(initial + N_backup, N_runs)
+	device = QasmSimulator.from_backend(backend_device)
+	coupling_map = device.configuration().coupling_map
+	noise_model = NoiseModel.from_backend(device)
+	basis_gates = noise_model.basis_gates
 
-	solvers_TPB = [VQE(ansatz, optimizer, initial_params, grouping='TPB',
-					   quantum_instance=qi, callback=callback) for _ in range(final - initial)]
+	qi = QuantumInstance(backend=backend_sim,
+	                     coupling_map=coupling_map,
+	                     noise_model=noise_model,
+	                     basis_gates=basis_gates,
+	                     shots=NUM_SHOTS)
+	# measurement_error_mitigation_cls=CompleteMeasFitter)
+	# cals_matrix_refresh_period=15)
 
-	solvers_EM = [VQE(ansatz, optimizer, initial_params, grouping='Entangled',
-					  quantum_instance=qi, callback=callback) for _ in range(final - initial)]
+	data_groups = np.load('../data/optimal_grouping_' + molecule_name + '_' + name_backend + '.npy',
+	                      allow_pickle=True).item()
 
-	solvers_HEEM = [VQE(ansatz, optimizer, initial_params, grouping='Entangled',
-						quantum_instance=qi, connectivity=coupling_map, callback=callback) for _ in
-					range(final - initial)]
+	qubit_op = data_groups['qubit_op']
 
-	energies_TPB = Parallel(n_jobs=-1)(delayed(run_VQE)(solver, qubit_op, None) for solver in solvers_TPB)
-	print('TPB Completed')
-	energies_EM = Parallel(n_jobs=-1)(delayed(run_VQE)(solver, qubit_op, None) for solver in solvers_EM)
-	print('EM Completed')
-	energies_HEEM = Parallel(n_jobs=-1)(delayed(run_VQE)(solver, qubit_op, None) for solver in solvers_HEEM)
-	print('HEEM Completed')
+	result_exact = NumPyMinimumEigensolver().compute_minimum_eigenvalue(qubit_op)
 
-	if save:
+	qubit_op_TPB = change_order_qubitop(qubit_op, data_groups['TPB']['paulis_order'], data_groups['TPB']['qubit_order'])
+	qubit_op_EM = change_order_qubitop(qubit_op, data_groups['EM']['paulis_order'], data_groups['EM']['qubit_order'])
+	qubit_op_HEEM = change_order_qubitop(qubit_op, data_groups['HEEM']['paulis_order'],
+	                                     data_groups['HEEM']['qubit_order'])
 
-		if i == 0:
+	print('The exact energy is: {:.3f}'.format(result_exact.eigenvalue.real))
+
+	init_state = data_groups['TPB']['init_state']
+	num_qubits = qubit_op.num_qubits
+	ansatz = init_state.compose(EfficientSU2(num_qubits, ['ry', 'rz'], entanglement='linear', reps=1))
+	num_var = ansatz.num_parameters
+	initial_params = [0.1] * num_var
+
+	optimizer = optimizer(maxiter=maxiter)
+
+	if clean_start and save:
+		try:
+			os.remove('../data/' + file_name + '.npy')
+		except FileNotFoundError:
+			pass
+
+	N_batches = int(N_runs / n_jobs)
+
+	pbar = tqdm(total=N_batches, desc='Computing VQE', file=sys.stdout, ncols=90,
+	            bar_format='{l_bar}{bar}{r_bar}')
+	for i in range(N_batches):
+		initial = i * n_jobs
+		final = min(initial + n_jobs, N_runs)
+
+		solvers_TPB = [VQE(ansatz, optimizer, initial_params, grouping='TPB',
+		                   quantum_instance=qi, callback=callback, Groups=data_groups['TPB']['Groups'],
+		                   Measurements=data_groups['TPB']['Measurements']) for _ in range(final - initial)]
+
+		solvers_EM = [VQE(ansatz, optimizer, initial_params, grouping='Entangled',
+		                  quantum_instance=qi, callback=callback, Groups=data_groups['EM']['Groups'],
+		                  Measurements=data_groups['EM']['Measurements'], layout=data_groups['EM']['T']) for _ in
+		              range(final - initial)]
+
+		solvers_HEEM = [VQE(ansatz, optimizer, initial_params, grouping='Entangled',
+		                    quantum_instance=qi, connectivity=coupling_map, callback=callback,
+		                    Groups=data_groups['HEEM']['Groups'],
+		                    Measurements=data_groups['HEEM']['Measurements'], layout=data_groups['HEEM']['T']) for _ in
+		                range(final - initial)]
+
+		energies_TPB = Parallel(n_jobs=n_jobs, backend=backend_parallel)(
+			delayed(run_VQE)(solver, qubit_op_TPB, None) for solver in solvers_TPB)
+		print('TPB Completed')
+		energies_EM = Parallel(n_jobs=n_jobs, backend=backend_parallel)(
+			delayed(run_VQE)(solver, qubit_op_EM, None) for solver in solvers_EM)
+		print('EM Completed')
+		energies_HEEM = Parallel(n_jobs=n_jobs, backend=backend_parallel)(
+			delayed(run_VQE)(solver, qubit_op_HEEM, None) for solver in solvers_HEEM)
+		print('HEEM Completed')
+
+		if save:
 			try:
 				data = np.load('../data/' + file_name + '.npy', allow_pickle=True).item()
+				print('Loaded previous data')
+				start = 0
+			except FileNotFoundError:
+				data = {'TPB': energies_TPB[0], 'EM': energies_EM[0], 'HEEM': energies_HEEM[0]}
+				print('New data created')
+				start = 1
 
-				if len(data['TPB'][0]) < len(energies_TPB[0]):
-					os.remove('../data/' + file_name + '.npy')
-				else:
-					save = question_overwrite(file_name)
+			for j in range(start, len(energies_HEEM)):
+				data['TPB'] = np.vstack([data['TPB'], energies_TPB[j]])
+				data['EM'] = np.vstack([data['EM'], energies_EM[j]])
+				data['HEEM'] = np.vstack([data['HEEM'], energies_HEEM[j]])
 
-			except Exception:
-				pass
+			save_object(data, file_name, overwrite=True)
 
-		try:
-			data = np.load('../data/' + file_name + '.npy', allow_pickle=True).item()
-			print('Loaded previous data')
-			start = 0
-		except:
-			data = {'TPB': energies_TPB[0], 'EM': energies_EM[0], 'HEEM': energies_HEEM[0]}
-			print('New data created')
-			start = 1
+		print('-' * 90)
+		pbar.update()
+	pbar.close()
 
-		for j in range(start, len(energies_HEEM)):
-			data['TPB'] = np.vstack([data['TPB'], energies_TPB[j]])
-			data['EM'] = np.vstack([data['EM'], energies_EM[j]])
-			data['HEEM'] = np.vstack([data['HEEM'], energies_HEEM[j]])
-
-		save_object(data, file_name, overwrite=True)
-
-	print('-' * 90)
-	pbar.update()
-pbar.close()
+	print('Code ends at:', current_time())
