b3b8f51a2206c3e8b4a5ea6940434a49bf6909cc
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2022-05-31 08:55:19 +0200

    Change from QuantumInstance to AreSimulator

--- Codes/examples/energy_evaluation.py
+++ Codes/examples/energy_evaluation.py
@@ -2,257 +2,122 @@ import sys
 import warnings
 
 sys.path.append('../')
+warnings.filterwarnings('ignore')
 
 import numpy as np
-import networkx as nx
-import os
-from tqdm import tqdm
-from joblib import Parallel, delayed
+from tqdm.auto import tqdm
+from os import cpu_count
 import getopt
-from time import time
 
-from qiskit import Aer, QuantumCircuit, IBMQ
-from qiskit.quantum_info import random_statevector
-from qiskit.providers.aer import QasmSimulator
-from qiskit.providers.aer.noise import NoiseModel
-from qiskit.utils.quantum_instance import QuantumInstance
-from qiskit.compiler import transpile
-from qiskit.ignis.verification import get_ghz_simple
+from qiskit import IBMQ
+from qiskit.providers.aer import AerSimulator
 
-from utils import molecules, Label2Chain, get_backend_connectivity, save_object, number_cnots
-from HEEM_VQE_Functions import measure_circuit_factor, probability2expected, post_process_results
-from GroupingAlgorithm import groupingWithOrder, TPBgrouping
-
-os.environ['QISKIT_IN_PARALLEL'] = 'TRUE'
+from utils import molecules, Label2Chain, load_grouping_data
+from HEEM_VQE_Functions import measure_circuit_factor, probability2expected_binary, post_process_results_3
 
 
-def compute_energy_exact(seed=None):
-    np.random.seed(seed)
-    state_0 = QuantumCircuit(n_qubits)
-    # state_0.initialize(random_statevector(2 ** n_qubits).data)
-    state_0 = state_0.compose(get_ghz_simple(n_qubits, measure=False))  # Initialized in the GHZ state
+def compute_energy(groups, measurements, batch_size, layout=None):
+    circuits = [measure_circuit_factor(measurement, n_qubits, measure_all=False) for measurement in measurements]
+    shots = NUM_SHOTS // len(groups)
 
-    # Exact
-    _, Groups_TPB, Measurements_TPB = TPBgrouping(paulis)
-    prob2Exp_TPB = probability2expected(coeff, labels, Groups_TPB, Measurements_TPB)
+    if batch_size == -1:
+        batch_size = len(circuits)
+    n_batches = int(np.ceil(len(circuits) / batch_size))
 
-    circuits_TPB = []
-    for j, measure in enumerate(Measurements_TPB):
-        circuit = measure_circuit_factor(measure, n_qubits, make_measurements=False)
-        circuit = circuit.compose(state_0, front=True)
-        circuit.save_statevector()
-        circuits_TPB.append(circuit)
-    circuits_TPB = transpile(circuits_TPB, simulator)
+    counts = []
+    pbar = tqdm(range(len(circuits)), desc='Simulating circuits')
+    for i in range(n_batches):
+        initial = i * batch_size
+        final = min(initial + batch_size, len(circuits))
 
-    result_TPB = simulator.run(circuits_TPB).result()
-    exact_energy = 0
-    for j in range(len(Measurements_TPB)):
-        prob = np.abs(result_TPB.get_statevector(circuits_TPB[j])) ** 2
-        exact_energy += np.sum(prob2Exp_TPB[j] @ prob)
+        if layout is not None:
+            temp = simulator.run(circuits[initial:final], shots=shots,
+                                 initial_layout=layout[::-1]).result().get_counts()
+        else:
+            temp = simulator.run(circuits[initial:final], shots=shots).result().get_counts()
 
-    return exact_energy, state_0
+        if type(temp) != list:
+            temp = [temp]
+        counts += temp
 
+        pbar.update(final - initial)
+    pbar.close()
 
-def compute_energy(qi, circuits, state_0, prob2Exp):
-    circuits = [circuit.compose(state_0, front=True) for circuit in circuits]
+    energy_temp = []
+    pbar = tqdm(range(len(circuits)), desc='Computing energy')
+    for i in pbar:
+        counts_indices, counts_values = post_process_results_3(counts[i])
 
-    counts = qi.execute(circuits).get_counts()
-    if len(circuits) == 1:
-        counts = [counts]
-    probabilities = [post_process_results(counts[j], circuits[j].num_clbits, qi.run_config.shots) for j in
-                     range(len(counts))]
-    energy = 0
-    for j in range(len(probabilities)):
-        energy += np.sum(prob2Exp[j] @ probabilities[j])
+        diagonals, factors = probability2expected_binary(coeffs, labels, [groups[i]], [measurements[i]], shift=False)
+        diagonals = [(~diagonal * 2 - 1).astype('int8') for diagonal in diagonals[0][:, counts_indices]]
+        energy_temp.append(np.sum((diagonals * np.array(factors[0])[:, None]) * counts_values[None, :]) / shots)
 
-    return energy
+    return np.sum(energy_temp)
 
 
-def compare_grouping(initial_state, energy_exact, seed):
-    np.random.seed(seed)
+def compute_error(method):
+    print('\n')
+    print(f'Computing {method}')
 
-    energy_TPB = compute_energy(qi_TPB, circuits_TPB, initial_state, prob2Exp_TPB)
-    error_TPB = np.abs((energy_TPB - energy_exact) / energy_exact)
+    if method == 'TPB':
+        Groups, Measurements = load_grouping_data(molecule_name, method)
+        Layout = None
+    else:
+        Groups, Measurements, Layout = load_grouping_data(molecule_name, method)
 
-    energy_EM = compute_energy(qi_EM, circuits_EM, initial_state, prob2Exp_EM)
-    error_EM = np.abs((energy_EM - energy_exact) / energy_exact)
+    print('Number of groups:', len(Groups))
 
-    energy_HEEM = compute_energy(qi_HEEM, circuits_HEEM, initial_state, prob2Exp_HEEM)
-    error_HEEM = np.abs((energy_HEEM - energy_exact) / energy_exact)
+    energy = compute_energy(Groups, Measurements, Batch_size, Layout)
+    error = np.abs((energy - energy_exact) / energy_exact)
 
-    return error_TPB, error_EM, error_HEEM
+    print('Relative error: {:.3f} %'.format(error * 100))
 
-
-def cnots(qi, measurements):
-    circuits = []
-    for measure in measurements:
-        circuit = measure_circuit_factor(measure, n_qubits)
-        circuits.append(circuit)
-
-    return number_cnots(circuits, qi)
-
-
-# ------------  Default parameters calculation  --------------------
-molecule_name = 'C2H2'
-n_jobs = 12
-NUM_SHOTS = 2 ** 14
-n_states = 12
-
-name_backend = 'ibmq_montreal'
-backend_parallel = 'loky'
-noise = True
-# ---------------------------------------------------------
-message_help = 'energy_evaluation.py -m <molecule ({})> -j <#JOBS ({})> -s <# shots ({})> ' \
-               '-N <# runs ({})> -n <noise ({}),'.format(molecule_name, n_jobs, NUM_SHOTS, n_states, noise)
 
 try:
     argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "hm:j:s:N:n:")
+    opts, args = getopt.getopt(argv, "m:")
 except getopt.GetoptError:
-    print(message_help)
+    print('Wrong argument.')
     sys.exit(2)
 
+molecule_name = 'C2H2'
 for opt, arg in opts:
-    if opt == '-h':
-        print(message_help)
-        sys.exit(2)
-    elif opt == '-m':
+    if opt == '-m':
         molecule_name = str(arg)
-    elif opt == '-j':
-        n_jobs = int(arg)
-    elif opt == '-s':
-        NUM_SHOTS = int(arg)
-    elif opt == '-N':
-        n_states = int(arg)
-    elif opt == '-n':
-        if arg == 'True':
-            noise = True
-        elif arg == 'False':
-            noise = False
-        else:
-            raise Exception('{} is an incorrect value for the noise model boolean.'.format(arg))
 
-if __name__ == '__main__':
-    start = time()
-    file_name = 'energy_evaluation_GHZ_' + molecule_name
+print('The chosen molecule is ' + molecule_name)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    IBMQ.load_account()
 
-    if n_jobs == -1:
-        n_jobs = os.cpu_count()
+provider_CSIC = IBMQ.get_provider(hub='ibm-q-csic', group='internal', project='iff-csic')
 
-    n_states = int(np.ceil(n_states / n_jobs) * n_jobs)
+name_backend = 'ibmq_montreal'
+backend = provider_CSIC.get_backend(name_backend)
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        IBMQ.load_account()
+simulator = AerSimulator.from_backend(backend)  # Backend for simulation
+simulator.set_options(max_parallel_experiments=0)
 
-    provider = IBMQ.get_provider(hub='ibm-q-csic', group='internal', project='iff-csic')
-    backend_device = provider.get_backend(name_backend)
-    WC_device = get_backend_connectivity(backend_device)
-    simulator = Aer.get_backend('aer_simulator')  # Backend for simulation
+NUM_SHOTS = 2 ** 22
+print(f'Total number of shots (equally distributed across circuits): {NUM_SHOTS}')
+# batch_size = cpu_count() * 4
+Batch_size = -1
 
-    device = QasmSimulator.from_backend(backend_device)
+qubit_op = molecules(molecule_name)
+paulis, coeffs, labels = Label2Chain(qubit_op)
 
-    coupling_map = device.configuration().coupling_map
-    # coupling_map = None
+n_qubits = qubit_op.num_qubits
 
-    if noise:
-        noise_model = NoiseModel.from_backend(device)
-        basis_gates = noise_model.basis_gates
-        print('Noise model')
-    else:
-        noise_model = None
-        basis_gates = None
-        print('Noise free')
+print(f'{len(paulis)} total Pauli strings')
+print(f'{n_qubits} qubits')
 
-    print('Comparing energy for', molecule_name)
-    try:
-        qubit_op = np.load('../data/old/big_molecules.npy', allow_pickle=True).item()[molecule_name]
-        print('Data loaded')
-    except KeyError:
-        qubit_op = molecules(molecule_name)
+energy_exact = 0
+for i in range(len(labels)):
+    label = labels[i]
+    if 'X' not in label and 'Y' not in label:
+        energy_exact += coeffs[i]
+print('\nExact energy: {}'.format(energy_exact))
 
-    paulis, coeff, labels = Label2Chain(qubit_op)
-
-    n_qubits = qubit_op.num_qubits
-
-    G_device = nx.Graph()
-    G_device.add_edges_from(WC_device)
-
-    _, Groups_TPB, Measurements_TPB = TPBgrouping(paulis)
-    Groups_EM, Measurements_EM, layout_EM = groupingWithOrder(paulis, connected=True)
-    Groups_HEEM, Measurements_HEEM, layout_HEEM = groupingWithOrder(paulis, G_device, connected=True)
-
-    prob2Exp_TPB = probability2expected(coeff, labels, Groups_TPB, Measurements_TPB)
-    prob2Exp_EM = probability2expected(coeff, labels, Groups_EM, Measurements_EM)
-    prob2Exp_HEEM = probability2expected(coeff, labels, Groups_HEEM, Measurements_HEEM)
-
-    circuits_TPB = [measure_circuit_factor(measurement, n_qubits) for measurement in Measurements_TPB]
-    circuits_EM = [measure_circuit_factor(measurement, n_qubits) for measurement in Measurements_EM]
-    circuits_HEEM = [measure_circuit_factor(measurement, n_qubits) for measurement in Measurements_HEEM]
-
-    # TPB
-    qi_TPB = QuantumInstance(backend=simulator, coupling_map=coupling_map, noise_model=noise_model,
-                             basis_gates=basis_gates, shots=NUM_SHOTS // len(Groups_TPB))
-    n_cnots_TPB = cnots(qi_TPB, Measurements_TPB)
-
-    # EM
-    qi_EM = QuantumInstance(backend=simulator, coupling_map=coupling_map, noise_model=noise_model,
-                            basis_gates=basis_gates, shots=NUM_SHOTS // len(Groups_EM))
-    qi_EM.set_config(initial_layout=layout_EM[::-1])
-    n_cnots_EM = cnots(qi_EM, Measurements_EM)
-
-    # HEEM
-    qi_HEEM = QuantumInstance(backend=simulator, coupling_map=coupling_map, noise_model=noise_model,
-                              basis_gates=basis_gates, shots=NUM_SHOTS // len(Groups_HEEM))
-    qi_HEEM.set_config(initial_layout=layout_HEEM[::-1])
-    n_cnots_HEEM = cnots(qi_HEEM, Measurements_HEEM)
-
-    # pbar = tqdm(range(n_states), desc='Loading exact energies', ncols=90)
-    # results = Parallel(n_jobs=n_jobs, backend=backend_parallel)(delayed(compute_energy_exact)(None) for _ in pbar)
-
-    # init_states = []
-    # 	# exact_energies = []
-    # 	# for i in range(n_states):
-    # 	# 	if np.abs(results[i][0]) > threshold:
-    # 	# 		exact_energies.append(results[i][0])
-    # 	# 		init_states.append(results[i][1])
-
-    exact_energy, init_state = compute_energy_exact()
-
-    # N = len(exact_energies)
-    N = n_states
-    pbar = tqdm(range(N), desc='Comparing groupings', ncols=90)
-    if n_jobs == 1:
-        results = []
-        for i in range(N):
-            results.append(compare_grouping(init_state, exact_energy, None))
-            pbar.update()
-        pbar.close()
-    else:
-        results = Parallel(n_jobs=n_jobs, backend=backend_parallel)(
-            delayed(compare_grouping)(init_state, exact_energy, None) for i in pbar)
-
-    errors_TPB = np.zeros(N)
-    errors_EM = np.zeros(N)
-    errors_HEEM = np.zeros(N)
-    for i in range(N):
-        errors_TPB[i], errors_EM[i], errors_HEEM[i] = results[i]
-
-    print('Average error for TPB: {:.3f} %'.format(np.average(errors_TPB) * 100))
-    print('Average error for EM: {:.3f} %'.format(np.average(errors_EM) * 100))
-    print('Average error for HEEM: {:.3f} %'.format(np.average(errors_HEEM) * 100))
-
-    parameters = {'molecule': molecule_name, 'device': name_backend, 'NUM_SHOTS': NUM_SHOTS, 'n_states': n_states}
-    data = {'TPB': errors_TPB, 'EM': errors_EM, 'HEEM': errors_HEEM, 'parameters': parameters,
-            'n_cnots_TPB': n_cnots_TPB, 'n_cnots_EM': n_cnots_EM, 'n_cnots_HEEM': n_cnots_HEEM}
-
-    if noise_model is None:
-        file_name += '_noise_free'
-
-    save_object(data, file_name, overwrite=True)
-
-    print('With TPB there are a total of {} cnots gates'.format(n_cnots_TPB))
-    print('With EM there are a total of {} cnots gates'.format(n_cnots_EM))
-    print('With HEEM there are a total of {} cnots gates'.format(n_cnots_HEEM))
-
-    print('The code takes {:.3f} min'.format((time() - start) / 60))
+methods = ['TPB', 'EM', 'HEEM']
+for method in methods:
+    compute_error(method)
