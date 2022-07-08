7dd90f903d9fac53e1e5dba2a9525590b5f6dff1
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2022-07-08 11:10:49 +0200

    feature: Add IBMQ parallel
    Add functions to check the status of multiple IBMQ jobs, and send job in parallel to IBMQ.

--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
-from typing import List
+from typing import List, Tuple, Union, Optional
 import numpy as np
 import networkx as nx
 import copy
 import joblib
 import contextlib
+from tqdm.auto import tqdm
+import time
+
+from qiskit import execute
+from qiskit.circuit import QuantumCircuit
+from qiskit.providers import JobStatus, BaseBackend
+from qiskit.providers.ibmq.job import IBMQJob
+from qiskit.providers.backend import Backend
 
 
 def random_labels(n: int, N: int) -> np.ndarray:
@@ -80,6 +88,7 @@ def tqdm_joblib(tqdm_object):
     Context manager to patch joblib to report into tqdm progress bar given as argument. Code extracted from the
     stack overflow response: https://stackoverflow.com/a/58936697/8237804
     """
+
     class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
         def __call__(self, *args, **kwargs):
             tqdm_object.update(n=self.batch_size)
@@ -92,3 +101,194 @@ def tqdm_joblib(tqdm_object):
     finally:
         joblib.parallel.BatchCompletionCallBack = old_batch_callback
         tqdm_object.close()
+
+
+def check_status_jobs(running: List[Tuple[int, IBMQJob]], n_batches: int, verbose: Optional[bool] = False) -> Union[
+    None, Tuple[bool, int, int]]:
+    """
+    Check is some job is completed in order to send the next one. If some job has raised an error, then return it so
+    is sent again.
+    Parameters
+    ----------
+    running: list(tuple(int, IBMQJob)
+        List with the running jobs. Each element is a tuple where the first element is the job index, and the second one
+        the job itself.
+    n_batches: int
+        Total number of jobs to be sent. This number is only used if verbose=True
+    verbose: bool (optional, default=None)
+        If true, print when a job is completed, an error raised, of the job have been cancelled.
+    Returns
+    -------
+    completed: bool
+        If the job have been completed, then completed=True. If the job is cancelled or raised some error,
+        completed=False. Otherwise, completed=None.
+    job_index: int
+        Index of the completed/cancelled job
+    queue_index: int
+        Index of the completed/cancelled job within the running jobs.
+    """
+    for j, (index, job) in enumerate(running):
+        if verbose:
+            print(f'Checking job {index + 1}/{n_batches}')
+
+        status = job.status()
+        if status == JobStatus.DONE:
+            if verbose:
+                print(f'Job {index + 1}/{n_batches} done')
+            return True, j, index
+        elif status == JobStatus.CANCELLED:
+            if verbose:
+                print(f'Job {index + 1}/{n_batches} cancelled')
+            return False, j, index
+        elif status == JobStatus.ERROR:
+            if verbose:
+                print(f'Job {index + 1}/{n_batches} failed')
+            return False, j, index
+
+
+def send_job_backend(backend: Union[BaseBackend, Backend], circuits_batch: List[QuantumCircuit],
+                     index: Optional[int] = 1, n_batches: Optional[int] = 1,
+                     job_tag: Optional[Union[str, List[str]]] = None, verbose: Optional[bool] = True,
+                     **kwargs_run: dict) -> IBMQJob:
+    """
+    Send a job to ibm with a series of circuits.
+    Parameters
+    ----------
+    backend: BaseBackend or Backend
+        IBMQ backend to send the job
+    circuits_batch: list(QuantumCircuits)
+        Circuits to execute in the backend
+    index: int (optional, default=1)
+        If multiple jobs are going to be sent to IBM, then use the name of the job to write the job index.
+    n_batches: int (optional, default=1)
+        if multiple jobs are going to be sent to IBM, then use the name of the job to write the total number of jobs.
+    kwargs_run: dict (optional, default=None)
+        Optional arguments for execute the circuits
+    job_tag: str (optional, default=None)
+        Job tag.
+    verbose: bool (optional, default=True)
+        If True, print when the job is sent, together with the job index and the total number of jobs to be sent.
+
+    Returns
+    -------
+    job: IBMQJob
+        Job sent to IBMQ.
+    """
+
+    job = execute(circuits_batch, backend, **kwargs_run)
+    if job_tag is not None:
+        if type(job_tag) is not list:
+            job_tag = [job_tag]
+
+        # TODO: Can be tags and name included in the execute command?
+        job.update_tags(additional_tags=job_tag)
+
+    job.update_name('{}/{}'.format(index + 1, n_batches))
+    if verbose:
+        print('Job {}/{} sent to IBMQ'.format(index + 1, n_batches))
+
+    return job
+
+
+def send_ibmq_parallel(backend: Union[BaseBackend, Backend], circuits: List[QuantumCircuit],
+                       job_tag: Optional[Union[str, List[str]]] = None, verbose: Optional[bool] = True,
+                       progress_bar: Optional[bool] = False, circuits_batch_size: Optional[int] = 300,
+                       n_jobs_parallel: Optional[int] = 5, waiting_time: Optional[int] = 20, **kwargs_run: dict) -> \
+        List[IBMQJob]:
+    """
+    Send to IBMQ backed multiples jobs backends, so they run in parallel. The circuits are divided in multiples batches.
+
+    Parameters
+    ----------
+    backend: BaseBackend or Backend
+        IBMQ backend to send the job
+    circuits: list(QuantumCircuit)
+        Circuits to be run
+    job_tag: str or list(str) (optional, default=None)
+        Tag for the job. If multiple tags, they can be added in a list.
+    verbose: bool (optional, default=True)
+        If True, write the progress as the jobs are sent, completed, cancelled, ...
+    progress_bar: bool (optional, default=False)
+        If True, print a progress bar as the jobs are completed.
+    circuits_batch_size: int (optional, default=300)
+        Maximum number of circuits in the batches. This can be limited by the backend options.
+        # TODO: Check if we can extract this information from backend, so
+        #  circuits_batch_size = max(backend.some_function(), circuits_batch_size)
+    n_jobs_parallel: int (optional, default=5)
+        Maximum number of jobs to be run in parallel.
+    waiting_time: int (optional, default=20
+        Waiting time (in sec) to wait between petitions to IBMQ for job monitor.
+    kwargs_run: dict (optional)
+        Extra optional arguments for the backend run.
+
+    Returns
+    -------
+    job_done: list(IBMQJob)
+        When all jobs are completed, they are returned in a sorted list. The circuits are located as follows:
+        job_1 -> [circuits[0], circuits[1], ..., circuits[circuits_batch_size - 1]]
+        job_2 -> [circuits[circuits_batch_size], ..., circuits[2 * circuits_batch_size - 1]]
+        .
+        .
+        .
+        job_-1 -> [..., circuits[-1]]
+    """
+    # TODO: Maybe we can check if there is some running jobs in the backend, and ask the user to cancel them.
+
+    n_circuits = len(circuits)
+    n_batches = int(np.ceil(n_circuits / circuits_batch_size))  # Number of circuits batches to sent
+
+    if n_jobs_parallel == -1:
+        n_jobs_parallel = n_batches
+
+    pbar = tqdm(total=n_batches, desc='Jobs completed', disable=not progress_bar)
+
+    # Indices for the first and last circuit in each batch
+    indices = []
+    for i in range(n_batches):
+        initial = i * circuits_batch_size
+        final = min(initial + circuits_batch_size, n_circuits)
+
+        indices.append((initial, final))
+
+    job_done = []
+    running_jobs = []
+
+    # Send the initial jobs
+    for i, index in enumerate(indices[:n_jobs_parallel]):
+        job = send_job_backend(backend, circuits[index[0]:index[1]], index=i, n_batches=n_batches, job_tag=job_tag,
+                               verbose=verbose, **kwargs_run)
+        running_jobs.append((i, job))
+
+    max_id_sent = n_jobs_parallel - 1
+
+    while len(running_jobs) > 0:  # Iterate until all jobs are completed
+        job_checker = check_status_jobs(running_jobs, n_batches, verbose=verbose)
+        if verbose:
+            print('Finish checking')
+
+        if job_checker is not None:  # If some job has been completed or cancelled
+            done, running_id, job_id = job_checker
+            if done:
+                pbar.update()
+                # job_done[job_id] = running_jobs.pop(running_id)[1]
+                job_done.append(running_jobs.pop(running_id))
+
+                if max_id_sent + 1 < n_batches:  # If there is still some job to be sent
+                    max_id_sent += 1
+                    job = send_job_backend(backend, circuits[indices[max_id_sent][0]:indices[max_id_sent][1]],
+                                           index=max_id_sent, n_batches=n_batches, job_tag=job_tag, verbose=verbose,
+                                           **kwargs_run)
+
+                    running_jobs.append((max_id_sent, job))
+            else:  # Resend the failed job
+                running_jobs.pop(running_id)
+                job = send_job_backend(backend, circuits[indices[job_id][0]:indices[job_id][1]], index=job_id,
+                                       n_batches=n_batches, job_tag=job_tag, verbose=verbose, **kwargs_run)
+                running_jobs.append((job_id, job))
+        else:
+            time.sleep(waiting_time)  # Wait some time for the next status, so IBMQ do not detect lots of petitions
+    pbar.close()
+
+    job_done = [x[1] for x in sorted(job_done, key=lambda x: x[0])]  # Sort the jobs using their job_id
+
+    return job_done
